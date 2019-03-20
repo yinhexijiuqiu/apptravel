337fb329800654f8dc5f8aec08c01df426ebebf9
zhangjy <183466911@qq.com>
2019-03-20 23:26:59 +0800

    igno

--- src/config/rem.js
+++ src/config/rem.js
@@ -1,1 +1,1 @@
-(function(){function a(){var b=document.documentElement.clientWidth;b=b>750?750:b;var c=b/750*100;document.getElementsByTagName("html")[0].style.fontSize=c+"px"}a();window.onresize=a})();
+(function() {function a() {var b = document.documentElement.clientWidth; b = b > 750 ? 750 : b;var c=b/750*100; document.getElementsByTagName("html")[0].style.fontSize = c + "px"}a(); window.onresize = a})();
