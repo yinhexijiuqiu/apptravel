130b51cf496c549e3ee37a33dc48bf7639f39975
zhangjy <183466911@qq.com>
2019-03-20 15:26:45 +0800

    adddetaillist

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
   import DetailHeader from './components/header';
   import DetailBanner from './components/banner';
+  import DetailList from './components/list';
   export default {
     name: 'Detail',
     components: {
       DetailBanner,
-      DetailHeader
+      DetailHeader,
+      DetailList
+    },
+    data() {
+      return {
+        list: [{
+          title: '成人票',
+          children: [{
+            title: '二级目录',
+            children: [{
+              title: '三级目录'
+            }]
+          }, {
+            title: '二级目录'
+          }]
+        }, {
+          title: '学生票'
+        }, {
+          title: '儿童票'
+        }, {
+          title: '特价票'
+        }]
+      };
     }
   };
 </script>
